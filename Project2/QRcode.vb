0d8e27a3a98c0e59e69bf7106623c85a3ec46796
phamngocson6596 <phamngocson6596@gmail.com>
2023-07-02 20:43:06 +0700

    lkj

--- Project2/QRcode.vb
+++ Project2/QRcode.vb
@@ -5,6 +5,7 @@ Imports Microsoft.Office.Interop.Word
 Imports ZXing
 Imports MongoDB.Bson
 Imports MongoDB.Driver
+Imports MongoDB.Driver.Core.Configuration
 
 Public Class QRcode
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
@@ -73,24 +74,6 @@ Public Class QRcode
         TextBox1.SelectAll()
     End Sub
 
-    Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
-        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
-            Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
-            Dim filePath As String = files(0) ' Assuming only one file is dropped
-
-            ' Read the QR code
-            Dim barcodeReader As New BarcodeReader()
-            Dim result As Result = barcodeReader.Decode(New Bitmap(filePath))
-
-            If result IsNot Nothing Then
-                Dim qrCodeText As String = result.Text
-                TextBox1.Text = qrCodeText
-            Else
-                Label1.Text = "Failed to read QR code"
-            End If
-        End If
-    End Sub
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Dim a As New OpenFileDialog
         If a.ShowDialog() = DialogResult.Cancel Then Exit Sub
@@ -111,32 +94,87 @@ Public Class QRcode
 
     End Sub
 
-
-    Private Sub PictureBox1_DragDrop(sender As Object, e As DragEventArgs)
-        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
-            Dim files As String() = CType(e.Data.GetData(DataFormats.FileDrop), String())
-            Dim filePath As String = files(0) ' Assuming only one file is dropped
-
-            ' Display the dragged image
-            'PictureBox1.Image = Image.FromFile(filePath)
-
-            ' Read the QR code
-            Dim barcodeReader As New BarcodeReader()
-            'Dim result As Result = barcodeReader.Decode(DirectCast(PictureBox1.Image, Bitmap))
-
-            'If result IsNot Nothing Then
-            'Dim qrCodeText As String = result.Text
-            'Label1.Text = qrCodeText
-            'Else
-            'Label1.Text = "Failed to read QR code"
-            'End If
-        End If
-    End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'My.Computer.Clipboard.SetText(QrAnalyzer(TextBox1.Text))
 
-        MongodbTesting()
+        'MongodbTesting()
 
+    End Sub
+    Private Sub SaveQr(text As String)
+        Dim regex As Regex = New Regex("\b(?<cccd>\d{12})\|(?<cmnd>\d*)\|(?<name>[^|]+)\|\d{4}(?<YoB>\d{4})\|(?<sex>(Nam|Nữ))\|(?<address>[^|]+)\|(?<dateOfIssue>\d+)\b")
+        Dim match As Match = regex.Match(text.TrimEnd)
+
+        If Not match.Success Then Exit Sub
+
+        Dim client As New MongoClient("mongodb+srv://tama:tama@tama.kzznzu2.mongodb.net/")
+        Dim database As IMongoDatabase = client.GetDatabase("Notary")
+        Dim collection As IMongoCollection(Of BsonDocument) = database.GetCollection(Of BsonDocument)("Customer")
+
+        Dim IDcard = New BsonDocument()
+        IDcard.Add("CCCD", match.Groups("cccd").Value)
+        If match.Groups("cmnd").Value.Length > 0 Then
+            IDcard.Add("CMND", match.Groups("cmnd").Value)
+        End If
+
+        Dim customer As New BsonDocument()
+        customer.Add("name", match.Groups("name").Value.ToUpper())
+
+        If match.Groups("sex").Value = "Nam" Then
+            customer.Add("sex", "Ông")
+        Else
+            customer.Add("sex", "Bà")
+        End If
+
+        customer.Add("YoB", match.Groups("YoB").Value)
+        customer.Add("IDcard", IDcard)
+        customer.Add("address", match.Groups("address").Value)
+        customer.Add("QrCode", text)
+
+        Dim filter As FilterDefinition(Of BsonDocument) = Builders(Of BsonDocument).Filter.AnyEq("IDcard.CCCD", "012069000031")
+
+
+    End Sub
+    Public Shared Sub Main(args As String())
+        ' Configure the MongoDB connection string and database name
+        Dim connectionString As String = "mongodb://localhost:27017"
+        Dim databaseName As String = "mydatabase"
+
+        ' Create a MongoClient to connect to the MongoDB server
+        Dim client As New MongoClient(connectionString)
+
+        ' Get a reference to the database
+        Dim database As IMongoDatabase = client.GetDatabase(databaseName)
+
+        ' Get a reference to the collection
+        Dim collection As IMongoCollection(Of BsonDocument) = database.GetCollection(Of BsonDocument)("mycollection")
+
+        ' Create the new document
+        Dim newDocument As New BsonDocument()
+        newDocument.Add("name", "BÙI MINH TRƯỜNG")
+        newDocument.Add("sex", "Ông")
+        newDocument.Add("YoB", "1969")
+        newDocument.Add("IDcard", New BsonDocument("CCCD", "012069000031").Add("CMND", "024370572"))
+        newDocument.Add("address", "29/27 Đoàn Thị Điểm, Phường 1, quận Phú Nhuận, TP. Hồ Chí Minh")
+
+        ' Check if a user with matching ID card exists
+        Dim filter As FilterDefinition(Of BsonDocument) = Builders(Of BsonDocument).Filter.AnyEq("IDcard.CCCD", "012069000031") Or
+                                                        Builders(Of BsonDocument).Filter.AnyEq("IDcard.CMND", "024370572")
+        Dim existingDocument As BsonDocument = collection.Find(filter).FirstOrDefault()
+
+        If existingDocument IsNot Nothing Then
+            ' Update the existing document
+            Dim updateFilter As FilterDefinition(Of BsonDocument) = Builders(Of BsonDocument).Filter.AnyEq("_id", existingDocument("_id"))
+            Dim update As UpdateDefinition(Of BsonDocument) = Builders(Of BsonDocument).Update.Set("name", newDocument("name"))
+
+            collection.UpdateOne(updateFilter, update)
+
+            Console.WriteLine("Document updated successfully.")
+        Else
+            ' Insert the new document
+            collection.InsertOne(newDocument)
+
+            Console.WriteLine("Document inserted successfully.")
+        End If
     End Sub
     Sub MongodbTesting()
         ' Configure the MongoDB connection string and database name
